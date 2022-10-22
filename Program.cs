813fe2ddce3d0ee16af91867a55d820f0ac55fb3
li-linfang <lilnfh25@163.com>
2022-10-23 02:41:14 +0800

    update folder structure
    1. Preceding folder structure is wrong, so I delete it and then update a new folder structure.
    
    2. Add screenshot function. Now, program can run properly.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using SpacedScreenshot.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacedScreenshot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            // Initialize object and it fileds
+            SpaceScreenshotObject spaceScreenshotObj = new SpaceScreenshotObject();
+
+            spaceScreenshotObj.TargetFolder = "D:\\ScreenshotTest";
+            spaceScreenshotObj.FolderStructureMethod = 0;
+
+            spaceScreenshotObj.ScreenshotFormat = 1;
+            spaceScreenshotObj.ScreenshotQuality = 90;
+            spaceScreenshotObj.ScreenshotPrefix = "prefix";
+            spaceScreenshotObj.ScreenshotNamingRule = 2;
+            spaceScreenshotObj.ScreenshotSuffix = "suffix";
+
+            spaceScreenshotObj.SpacedTime = 2;
+
+            spaceScreenshotObj.PauseWhenNotActiveEnable = true;
+            spaceScreenshotObj.NotActiveTime = 60;
+
+            spaceScreenshotObj.AutoCompressEnable = true;
+            spaceScreenshotObj.CompresssDay = 7;
+            spaceScreenshotObj.AutoDeleteCompressedPackageEnable = true;
+            spaceScreenshotObj.DeleteCompressedPackageDay = 30;
+
+            spaceScreenshotObj.Start();
+            /**
+            **/
+
+            //ScreenshotTest.testTakeScreenshot();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/ScreenshotTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace SpacedScreenshot.Test
+{
+    internal class ScreenshotTest
+    {
+        public static void testTakeScreenshot()
+        {
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+            var g = Graphics.FromImage(bmpScreenshot);
+
+            g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+
+            bmpScreenshot.Save("screenshot1.jpeg", ImageFormat.Jpeg);
+        }
+    }
+}
