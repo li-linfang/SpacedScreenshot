d2743747971a1fee5cfbf031a6dd9eaea51150ae
li-linfang <lilnfh25@163.com>
2022-10-23 00:36:56 +0800

    take screenshot function

---
+++ SpacedScreenshot/Test/ScreenshotTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SpacedScreenshot.Test
+{
+    internal class ScreenshotTest
+    {
+        public static void testTakeScreenshot()
+        {
+            //Bitmap bm = new Bitmap();
+            var width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+
+
+
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+        }
+    }
+}
